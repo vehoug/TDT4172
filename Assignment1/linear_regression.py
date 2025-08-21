94b9bbdc6c7929152ab39378b7fa03e757f7734b
Vegard Schøyen Houg <vegard.houg@revolve.no>
2025-08-21 09:59:02 +0200

    Initial commit
    Add files for first assignment

---
+++ Assignment1/linear_regression.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+class LinearRegression():
+    
+    def __init__(self):
+        # NOTE: Feel free to add any hyperparameters 
+        # (with defaults) as you see fit
+        pass
+        
+    def fit(self, X, y):
+        """
+        Estimates parameters for the classifier
+        
+        Args:
+            X (array<m,n>): a matrix of floats with
+                m rows (#samples) and n columns (#features)
+            y (array<m>): a vector of floats
+        """
+        # TODO: Implement
+        raise NotImplementedError("The fit method is not implemented yet.")
+    
+    def predict(self, X):
+        """
+        Generates predictions
+        
+        Note: should be called after .fit()
+        
+        Args:
+            X (array<m,n>): a matrix of floats with 
+                m rows (#samples) and n columns (#features)
+            
+        Returns:
+            A length m array of floats
+        """
+        # TODO: Implement
+        raise NotImplementedError("The predict method is not implemented yet.")
+
+
+
+
+
